fad1bf944064b3c035eb1326c497ffd367500b9d
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-12-18 23:18:06 +0000

    Start writing code that will actually place ships on board

---
+++ src/modules/components/gameboard.js
@@ -0,0 +1,24 @@
+import { setCurrentShipLength } from "../utils/helpers.js";
+import { isValidPlacement } from "../utils/helpers.js";
+
+const gameboard = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+function placeShip(x, y) {
+  let axis = 'call a fxn that returns axis-stored-value. let axis will just be axis = ...';
+  let shipLength = setCurrentShipLength();
+  let valid = isValidPlacement(x, y, shipLength, axis);
+  // if (valid) {
+
+  // }
+}
