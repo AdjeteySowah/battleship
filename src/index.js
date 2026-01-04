11aa5b2ee47caa94f49c0381d95c5e2dab352046
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2026-01-04 23:21:10 +0000

    feat(entry): add app entry point
    - call initWindowListener(), mount('start'), listenForAudioIconClick()
    inside init()
    - invoke init() on module load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import "./styles.css";
+import './styles.css';
+import {
+  initWindowListener,
+  mount,
+  listenForAudioIconClick,
+} from './modules/ui/controller.js';
+
+function init() {
+  initWindowListener();
+  mount('start');
+  listenForAudioIconClick();
+}
+
+init();
