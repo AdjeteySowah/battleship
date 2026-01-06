926410bcfe9674011bbfd084da40a98c1d690752
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2026-01-06 16:08:29 +0000

    feat: add an auto random computer coord generator

--- src/modules/components/ai.js
+++ src/modules/components/ai.js
@@ -1,5 +1,22 @@
-export function generateRandomCoordinate() {
+import { isValidPlacement } from '../utils/helpers.js';
+
+export function genRandomAttackCoord() {
   const x = Math.floor(Math.random() * 10);
   const y = Math.floor(Math.random() * 10);
   return [x, y];
+}
+
+export function genRandomPlacementCoord(length, axis, board, size) {
+  let x;
+  let y;
+  if (axis === 'x') {
+    x = Math.floor(Math.random() * 10);
+    y = Math.floor(Math.random() * (length + 1));
+  } else {
+    x = Math.floor(Math.random() * (length + 1));
+    y = Math.floor(Math.random() * 10);
+  }
+
+  const valid = isValidPlacement(x, y, length, axis, board, size).valid;
+  return valid ? [x, y] : genRandomPlacementCoord(length, axis, board, size);
 }
