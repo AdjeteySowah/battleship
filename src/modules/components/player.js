a00082c703c205245af660d01ad7ce78a22e7c7d
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2026-01-09 23:01:24 +0000

    fix: ensure attack methods returns a data/value

--- src/modules/components/player.js
+++ src/modules/components/player.js
@@ -5,14 +5,14 @@ export function createPlayer({ playerName, isComputer } = {}) {
   const gameboard = createGameboard();
 
   function manualAttack(opponent, x, y) {
-    opponent.receiveAttack(x, y);
+    return opponent.gameboard.receiveAttack(x, y);
   }
 
   function autoAttack(opponent) {
     const coord = genRandomAttackCoord();
     const x = coord[0];
     const y = coord[1];
-    opponent.receiveAttack(x, y);
+    return opponent.gameboard.receiveAttack(x, y);
   }
 
   return {
