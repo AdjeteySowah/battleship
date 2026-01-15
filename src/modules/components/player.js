5cfc99211d9a50ab94b3faee88ae8d40572ecdb3
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2026-01-15 23:47:37 +0000

    refactor: simplify player factory's attack method

--- src/modules/components/player.js
+++ src/modules/components/player.js
@@ -1,17 +1,9 @@
 import { createGameboard } from './gameboard.js';
-import { genRandomAttackCoord } from './ai.js';
 
 export function createPlayer({ playerName, isComputer } = {}) {
   const gameboard = createGameboard();
 
-  function manualAttack(opponent, x, y) {
-    return opponent.gameboard.receiveAttack(x, y);
-  }
-
-  function autoAttack(opponent) {
-    const coord = genRandomAttackCoord();
-    const x = coord[0];
-    const y = coord[1];
+  function attack(opponent, x, y) {
     return opponent.gameboard.receiveAttack(x, y);
   }
 
@@ -19,6 +11,6 @@ export function createPlayer({ playerName, isComputer } = {}) {
     playerName,
     isComputer,
     gameboard,
-    attack: isComputer ? autoAttack : manualAttack,
+    attack,
   };
 }
