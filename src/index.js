17004de78dd75c2d3c337bf3c99e656418a1c8eb
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2026-01-22 17:29:03 +0000

    feat: call welcome alert when page loads

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import {
   mount,
   listenForAudioIconClick,
 } from './modules/ui/controller.js';
+import { showWelcomeAlert } from './modules/utils/helpers.js';
+
+window.addEventListener('load', showWelcomeAlert);
 
 function init() {
   initWindowListener();
